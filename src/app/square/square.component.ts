5dca52bda2b7c1b9fc5185d1c5e5a1616782a5ce
Mahmoud Elbayoumi <mahmoud.albaiyoumi@gmail.com>
2021-01-20 14:58:11 +0200

    Initial Commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,24 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: [
+    `
+    button {
+      width: 100%;
+      height: 100%;
+      font-size: large;
+    }
+    `
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O' = 'X'
+
+}
