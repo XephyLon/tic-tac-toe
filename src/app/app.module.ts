9e9cce9c9079fb1fbcb370d40d509b6592e8e511
Mahmoud Elbayoumi <mahmoud.albaiyoumi@gmail.com>
2021-01-20 15:28:07 +0200

    changes theme.scss to styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { BoardComponent } from './board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { environment } from '../environments/environment';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
